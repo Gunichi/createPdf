04556d1cca2b0ce107b10cd02ebf726a57a25d3f
Gunichi <gunichi297@gmail.com>
2023-04-18 17:15:43 -0300

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,10 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
-import { StepsTheme as Steps } from "chakra-ui-steps";
-
-const theme = extendTheme({
-  components: {
-    Steps,
-  },
-});
-
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
   )
